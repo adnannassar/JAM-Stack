6203ea450c961ca2e550dc2602b539ee4b4ef6bd
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2020-08-07 11:33:35 +0200

    fix

--- src/_data/news.js
+++ src/_data/news.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 require('dotenv').config();
 module.exports  =  async function (){
     try {
-        const responce = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
+        const responce = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
         return responce.data;
     }catch(error){
         console.log(error);
